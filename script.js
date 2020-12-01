732b4454dfbdba394589fa4ceceabca79c24ae71
JamesPrestonFoundry <64635362+JamesPrestonFoundry@users.noreply.github.com>
2020-12-01 18:43:43 +0000

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function createTicket(form) {
     let url = 'https://thefoundry.zendesk.com/api/v2/tickets.json';
     let subject = "First Year Free Web Application";
     let body = "Completed via the website";
-    let ticketData = {"ticket": {"custom_fields": [{"id": 360015922239, "value": form.fname.value},{"id": 360015946660, "value": form.lname.value},{"id": 360015946680, "value": form.country.value},{"id": 360015946840, "value": form.school.value},{"id": 360015922399, "value": form.course.value}, {"id": 360015946860, "value": form.graduation.value} ], "Submitter email": form.email.value, "subject": subject , "comment": { "body": body}}};
+    let ticketData = {"request": {"custom_fields": [{"id": 360015922239, "value": form.fname.value},{"id": 360015946660, "value": form.lname.value},{"id": 360015946680, "value": form.country.value},{"id": 360015946840, "value": form.school.value},{"id": 360015922399, "value": form.course.value}, {"id": 360015946860, "value": form.graduation.value} ], "Submitter email": form.email.value, "subject": subject , "comment": { "body": body}}};
     
     zdAuthRequest('POST', url, ticketData);
 };
